fd21ed7dea4472acc58a244bec8e00dddd930c6b
Amy Moller <amy.moller@yahoo.com>
2020-05-13 10:00:23 -0600

    Add more tests

--- test/house_test.rb
+++ test/house_test.rb
@@ -5,6 +5,25 @@ require './lib/house'
 
 class HouseTest < Minitest::Test
   def test_it_exists
-    house = House.new( )
+    house = House.new("$400000", "123 sugar lane")
     assert_instance_of House, house
-  end 
+  end
+
+  def test_it_has_a_price
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal 400000, house.price
+  end
+
+  def test_it_has_an_address
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_it_has_rooms
+    house = House.new("$400000", "123 sugar lane")
+
+
+
+
+
+end
