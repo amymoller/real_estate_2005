92822e2c72aad4ec7e95487e9af106d1d5aa4463
Amy Moller <amy.moller@yahoo.com>
2020-05-13 10:40:13 -0600

    Add lass methods for house test iter 3

--- test/house_test.rb
+++ test/house_test.rb
@@ -10,16 +10,19 @@ class HouseTest < Minitest::Test
   end
 
   def test_it_has_a_price
+    skip
     house = House.new("$400000", "123 sugar lane")
     assert_equal 400000, house.price
   end
 
   def test_it_has_an_address
+    skip
     house = House.new("$400000", "123 sugar lane")
     assert_equal "123 sugar lane", house.address
   end
 
   def test_it_has_rooms
+    skip
     house = House.new("$400000", "123 sugar lane")
 
     house.add_room(room_1)
@@ -27,5 +30,46 @@ class HouseTest < Minitest::Test
     house.add_room(room_2)
     room_2 = Room.new(:bedroom, 11, '15')
   end
+
+  def test_it_is_above_market_average
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    refute house.above_market_average?
+  end
+
+  def test_it_has_new_rooms
+    skip
+    house = House.new("$400000", "123 sugar lane")
+
+    house.add_room(room_3)
+    room_3 = Room.new(:living_room, 25, '15')
+    house.add_room(room_4)
+    room_4 = Room.new(:basement, 30, '41')
+  end
+
+  def test_it_has_an_area
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    room1 = Room.new(:bedroom, 10, '13')
+    room2 = Room.new(:living_room, 15, '12')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    assert_equal 1900, house.area
+  end
+
+  def test_it_has_new_details
+    skip
+    house = House.new("$400000", "123 sugar lane")
+    house.details = {:price => 400000, :address => "123 sugar lane"}
+  end
+
+end 
+
+
+
+
+
+
 
 end
