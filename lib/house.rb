d5dc496f4264dbfa924db86e2d73503d5513d5de
Amy Moller <amy.moller@yahoo.com>
2020-05-13 10:39:26 -0600

    Add last methods for house class

--- lib/house.rb
+++ lib/house.rb
@@ -1,3 +1,4 @@
+require './lib/room'
 class House
 
   attr reader :price,
@@ -11,7 +12,19 @@ class House
   end
 
   def add_room
-    @rooms = (room_1, room_2)
+    @rooms = (room_1, room_2, room_3, room_4)
+  end
+
+  def area
+    @length * @width
+  end
+
+  def above_market_average?
+    if @price > $500000
+      true
+    else
+      false
+    end
   end
 
 end
